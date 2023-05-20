3fbda137526d2f93fdfc47a64d41dfa7bf2ae84b
taylorgrant <taylorgrant38@gmail.com>
2023-05-20 16:59:08 -0700

    overall usage

--- R/twitter_scrape.R
+++ R/twitter_scrape.R
@@ -7,16 +7,13 @@ source_python(here::here("py_functions", "twitter_search.py"))
 source(here::here("R", "helpers", "tweetnlp_functions.R"))
 
 # build query
-query <- build_twitter_query(text = "",username = "ddayen",since = "2023-05-10", until = "",retweet = "y",replies = "y")
+query <- build_twitter_query(text = "",username = "carlquintanilla",since = "2023-05-10", until = "",retweet = "y",replies = "y")
 
 # how many to get
 n = 40
 
 # run it
-out <- twitter_search(n, query)
-
-# clean the data 
-out <- out |> 
+out <- twitter_search(n, query) |> 
   mutate(datetime = lubridate::with_tz(datetime, tzone = "America/Los_Angeles"),
          tweet_mentions = sapply(str_extract_all(text, "@\\w+"), function(x) paste(x, collapse = ", ")),
          text = str_replace_all(text, "\n", ""),
@@ -29,19 +26,19 @@ tweetnlp <- reticulate::import("tweetnlp")
 # load transformer models  
 sentiment_model <- tweetnlp$load_model('sentiment')
 singleclass_model <- tweetnlp$load_model('topic_classification', multi_label=FALSE)
+emoji_model <- tweetnlp$load_model('emoji')
+emotion_model <- tweetnlp$load_model('emotion')
 
-# run the sentiment 
-url_pattern <- "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+# run the nlp
 out <- out |> 
-  mutate(
-         sentiment1 = map(str_replace_all(text, url_pattern, ""), tweetnlp_sentiment),
-  unnest(cols = sentiment)
-
-# run the topic classifier
-out <- out |> 
-  mutate(topic = map(text, tweetnlp_topic)) |> 
-  unnest(cols = topic)
+  mutate(sentiment = map(str_replace_all(text, url_pattern, ""), tweetnlp_sentiment),
+         top_emotion = map(str_replace_all(text, url_pattern, ""), tweetnlp_emotion),
+         topic = map(str_replace_all(text, url_pattern, ""), tweetnlp_topic),
+         emoji = map(str_replace_all(text, url_pattern, ""), tweetnlp_emoji)) |> 
+  unnest(cols = c(sentiment, top_emotion, topic, emoji))
 
 
-
-
+tmp <- tweetnlp_emotion(out$text[4])
+tibble(prob = unlist(tmp$probability, use.names = FALSE)) |> 
+  slice_max(order_by = prob, n = 1)
+which.max(data.frame(lapply(tmp$probability, function(x) t(data.frame(x)))))
