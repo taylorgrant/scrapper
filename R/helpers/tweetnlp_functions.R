e6ab5df6d0d3081889e5487766ebcc27eb8d17d3
taylorgrant <taylorgrant38@gmail.com>
2023-05-20 16:58:59 -0700

    adding emotion prediction

--- R/helpers/tweetnlp_functions.R
+++ R/helpers/tweetnlp_functions.R
@@ -1,9 +1,28 @@
 # sentiment function
 tweetnlp_sentiment <- function(text) {
-  sentiment_model$sentiment(text)$label
+  tmp <- sentiment_model$sentiment(text, return_probability=TRUE)
+  tmp <- cbind(sentiment = tmp$label, data.frame(lapply(tmp$probability, function(x) t(data.frame(x)))))
+}
+
+# emotion function
+tweetnlp_emotion <- function(text) {
+  emofn <- function() {
+    tibble(emotion_prob = unlist(tmp$probability, use.names = FALSE)) |> 
+    slice_max(order_by = emotion_prob, n = 1)
+  }
+  tmp <- emotion_model$emotion(text, return_probability=TRUE)
+  tmp <- cbind(emotion = tmp$label, emofn())
 }
 
 # single class classification function
 tweetnlp_topic <- function(text) {
   singleclass_model$topic(text)$label
 }
+
+# predict the emoji to end the tweet 
+tweetnlp_emoji <- function(text) {
+  emoji_model$emoji(text)$label
+}
+
+# to strip out urls when running sentiment
+url_pattern <- "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
