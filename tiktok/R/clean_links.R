de7740cda7d855ac6a98edbdf1a1cb1499f5b5f0
taylorgrant <taylorgrant38@gmail.com>
2023-12-11 20:29:47 -0800

    allowing for handle or hashtag

--- tiktok/R/clean_links.R
+++ tiktok/R/clean_links.R
@@ -1,14 +1,15 @@
 # read, clean, and filter urls from link gopher
-clean_links <- function(file_loc, handle) {
+clean_links <- function(file_loc, handle = NULL, hashtag = NULL) {
   handle <- stringr::str_remove_all(handle, "@") # remove at handle if attached
+  hashtag <- stringr::str_remove_all(hashtag, "#") # remove at handle if attached
   
   if (str_detect(file_loc, ".csv")) {
     tmp <- read_csv(file_loc) |>  
-      filter(str_detect(links, glue::glue("tiktok.com/@{handle}/video"))) |> 
+      filter(str_detect(links, pattern = regex("@[^/]+/video"))) |> 
       pull()
   } else {
     tmp <- readxl::read_excel(file_loc) |> 
-      filter(str_detect(links, glue::glue("tiktok.com/@{handle}/video"))) |> 
+      filter(str_detect(links, pattern = regex("@[^/]+/video")))  |> 
       pull()
   }
   
--- tiktok/R/tiktok_full.R
+++ tiktok/R/tiktok_full.R
@@ -1,14 +1,20 @@
 ## Functions to scrape TikTok ## 
 
-tiktok_full <- function(handle) {
+tiktok_full <- function(handle = NULL, hashtag = NULL) {
   pacman::p_load(tidyverse, janitor, here, glue)
   source(here("tiktok", "R", "clean_links.R"))
   source(here("tiktok", "R", "tiktok_scrape.R"))
   
   data_path <- here('tiktok','tiktok_data', "links")
-  filename <- dir(data_path, pattern = handle)
-  loc <- file.path(data_path, filename)
-  urls <- clean_links(loc, handle)
+  if (is.null(handle)) {
+    filename <- dir(data_path, pattern = hashtag)
+    loc <- file.path(data_path, filename)
+    urls <- clean_links(loc, hashtag)
+  } else {
+    filename <- dir(data_path, pattern = handle)
+    loc <- file.path(data_path, filename)
+    urls <- clean_links(loc, handle)
+  }
   
   #-- scrap tiktok urls
   out <- tiktok_scrape(urls)
@@ -18,12 +24,18 @@ tiktok_full <- function(handle) {
     arrange(desc(timestamp))
   
   # save files
-  saveRDS(out, here('tiktok', 'tiktok_data', "processed", glue::glue('{handle}-{Sys.Date()}.rds')))
-  openxlsx::write.xlsx(out, here('tiktok', 'tiktok_data', "processed", glue::glue('{handle}-{Sys.Date()}.xlsx')))
+  if (is.null(handle)) {
+    saveRDS(out, here('tiktok', 'tiktok_data', "processed", glue::glue('hashtag_{hashtag}-{Sys.Date()}.rds')))
+    openxlsx::write.xlsx(out, here('tiktok', 'tiktok_data', "processed", glue::glue('hashtag_{hashtag}-{Sys.Date()}.xlsx')))
+  } else {
+    saveRDS(out, here('tiktok', 'tiktok_data', "processed", glue::glue('{handle}-{Sys.Date()}.rds')))
+    openxlsx::write.xlsx(out, here('tiktok', 'tiktok_data', "processed", glue::glue('{handle}-{Sys.Date()}.xlsx')))
+  }
+  
 }
 
 # example 
-# tiktok_full("bmw")
+tiktok_full(hashtag = "bmw")
 
 
 
