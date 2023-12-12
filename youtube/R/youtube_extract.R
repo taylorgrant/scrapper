71d3e9957912ee795cd944a7df3ff38fc4f34f65
taylorgrant <taylorgrant38@gmail.com>
2023-12-11 22:47:49 -0800

    dropping like count; yt no longer includes?

--- youtube/R/youtube_extract.R
+++ youtube/R/youtube_extract.R
@@ -1,11 +1,10 @@
 # to get data out of the returned lists # 
 youtube_extract <- function(datalist) {
-  # get summary data about the video
+  # get summary data about the video (like count no longer included in data)
   summary <- purrr::map_dfr(datalist, 
                             magrittr::extract, 
                             c("uploader", "upload_date", "title", "description", "categories",
-                              "duration_string", "view_count", 
-                              "like_count", "comment_count", "original_url")) |> 
+                              "duration_string", "view_count", "comment_count", "original_url")) |> 
     dplyr::rename(duration = duration_string) |> 
     dplyr::mutate(upload_date = lubridate::ymd(upload_date))
   # get the comments
