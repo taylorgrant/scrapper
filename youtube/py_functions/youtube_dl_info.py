dffe6f0ec32f42bae1a3e0e2ad333e5e9595195a
taylorgrant <taylorgrant38@gmail.com>
2023-12-11 22:48:41 -0800

    cleaning a bit

--- youtube/py_functions/youtube_dl_info.py
+++ youtube/py_functions/youtube_dl_info.py
@@ -2,8 +2,6 @@ def youtube_dl_info(URL):
   import json
   import yt_dlp
 
-  # URL = 'https://www.youtube.com/watch?v=Lz33OZf4Yx0'
-
   # See help(yt_dlp.YoutubeDL) for a list of available options and public functions
   ydl_opts = {'getcomments':True}
   with yt_dlp.YoutubeDL(ydl_opts) as ydl:
