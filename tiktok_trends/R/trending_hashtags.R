16bf11d38aa82ba9838b0db8dce94566ac616a6d
taylorgrant <taylorgrant38@gmail.com>
2023-12-13 11:17:06 -0800

    accounting for date range

--- tiktok_trends/R/trending_hashtags.R
+++ tiktok_trends/R/trending_hashtags.R
@@ -1,10 +1,10 @@
 # Tiktok Hashtag Data Pull # 
-trending_hashtags <- function(hashtag) {
+trending_hashtags <- function(hashtag, range) {
   
   pacman::p_load(tidyverse, here, glue, janitor, httr, rvest)
   
   get_data <- function(hashtag) {
-    url <- glue::glue("https://ads.tiktok.com/business/creativecenter/hashtag/{hashtag}/pc/en?rid=r349r7pp61&period=1095&countryCode=US")
+    url <- glue::glue("https://ads.tiktok.com/business/creativecenter/hashtag/{hashtag}/pc/en?rid=r349r7pp61&period={range}&countryCode=US")
     page <- session(url)
     out <- page |> 
       html_elements("script#__NEXT_DATA__") |> 
@@ -13,10 +13,12 @@ trending_hashtags <- function(hashtag) {
     
     dat <- out$props$pageProps$data
     trend <- dat$trend |> 
-      mutate(time = lubridate::as_datetime(time))
-    post_count_last_3yrs <- dat$publishCnt
+      mutate(time = lubridate::as_datetime(time),
+             time = as.Date(time),
+             value = value*100)
+    post_count <- dat$publishCnt
     total_post_count <- dat$publishCntAll
-    video_views_last_3yrs <- dat$videoViews
+    video_views <- dat$videoViews
     total_video_views <- dat$videoViewsAll
     audience_ages <- dat$audienceAges |> 
       mutate(ageLevel = case_when(ageLevel == 3 ~ "18-24",
@@ -30,16 +32,17 @@ trending_hashtags <- function(hashtag) {
                                score = dat$audienceCountries$score)
     related_hashtags <- dat$relatedHashtags$hashtagName
     
-    list(trend = trend, post_count_last_3yrs = post_count_last_3yrs, total_post_count = total_post_count,
-         video_views_last_3yrs = video_views_last_3yrs, total_video_views = total_video_views,
+    list(trend = trend, post_count = post_count, total_post_count = total_post_count,
+         video_views = video_views, total_video_views = total_video_views,
          audience_ages = audience_ages, audience_interests = audience_interests, 
          audience_country = audience_country, related_hashtags = related_hashtags)
   }
   
   data <- get_data(hashtag)
-
-  get_trend <-  function(hashtag) {
-    url <- glue::glue("https://ads.tiktok.com/business/creativecenter/hashtag/{hashtag}/pc/en?rid=r349r7pp61&period=1095&countryCode=US")
+  
+  get_trend <-  possibly(function(hashtag, range) {
+    
+    url <- glue::glue("https://ads.tiktok.com/business/creativecenter/hashtag/{hashtag}/pc/en?rid=r349r7pp61&period={range}&countryCode=US")
     page <- session(url)
     out <- page |> 
       html_elements("script#__NEXT_DATA__") |> 
@@ -47,19 +50,39 @@ trending_hashtags <- function(hashtag) {
       jsonlite::fromJSON()
     dat <- out$props$pageProps$data
     trend <- dat$trend |> 
-      mutate(time = lubridate::as_datetime(time))
-  }
-  related_trends <- data$related_hashtags |> 
-    map(get_trend) |> 
-    set_names(nm = data$related_hashtags) %>% 
+      mutate(time = lubridate::as_datetime(time),
+             time = as.Date(time),
+             value = value*100)
+    
+    stats <- tibble(post_count = dat$publishCnt,
+                    total_post_count = dat$publishCntAll,
+                    video_views = dat$videoViews,
+                    total_video_views = dat$videoViewsAll)
+    list(trend = trend, stats = stats)
+  }, otherwise = NA)
+  
+  related <- crossing(hashtag = data$related_hashtags, range)
+  related_data <- map2(related$hashtag, related$range, get_trend) |> 
+    set_names(nm = data$related_hashtags)
+  
+  # drop NA if necessary
+  related_data <- related_data[!is.na(related_data)]
+  
+  related_trends <- related_data |> map("trend") %>% 
     data.table::rbindlist(. , idcol = 'hashtag')
   
-  data <- c(data, list(related_trends = related_trends))
- 
+  related_stats <- related_data |> map("stats") %>%   
+    data.table::rbindlist(. , idcol = 'hashtag')
+  
+  hash1 <- tibble(hashtag = hashtag, 
+                  post_count = data$post_count,
+                  total_post_count = data$total_post_count,
+                  video_views = data$video_views,
+                  total_video_views = data$total_video_views)
+  related_stats <- rbind(hash1, related_stats) |> 
+    mutate(hashtag = paste0("#", hashtag))
+  
+  data <- c(data, list(related_trends = related_trends, related_stats = related_stats))
+  
 }
-
-
-
-
-
 
